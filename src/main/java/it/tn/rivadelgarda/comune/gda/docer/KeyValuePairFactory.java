0e72ea6cfb8d822e74f327086d3f14e19f12612a
Mirco <amirco@gmail.com>
2016-11-18 16:20:01 +0100

    aggiornamento metodi creazione parametri keyvalue

--- src/main/java/it/tn/rivadelgarda/comune/gda/docer/KeyValuePairFactory.java
+++ src/main/java/it/tn/rivadelgarda/comune/gda/docer/KeyValuePairFactory.java
@@ -1,26 +1,43 @@
 package it.tn.rivadelgarda.comune.gda.docer;
 
+import java.awt.image.Raster;
+import java.util.ArrayList;
+import java.util.List;
+
 import it.kdm.docer.webservices.DocerServicesStub.KeyValuePair;
 
 public class KeyValuePairFactory {
 
-	public final static String FOLDER_NAME = "FOLDER_NAME";
-	public final static String FOLDER_OWNER = "FOLDER_OWNER";
-	public final static String COD_AOO = "COD_AOO";
-	public final static String COD_ENTE = "COD_ENTE";
-	public final static String DES_FOLDER = "DES_FOLDER";
-	public final static String ENABLED = "ENABLED";
-	public final static String PARENT_FOLDER_NAME = "PARENT_FOLDER_NAME";
-	public final static String FOLDER_ID = "FOLDER_ID";
-	public final static String PARENT_FOLDER_ID = "PARENT_FOLDER_ID";
-	
 	public final static String ASC = "ASC";
 	public final static String DESC = "DESC";
 
-	public static KeyValuePair create(String key, String value) {
+	public static KeyValuePair createKey(String key, String value) {
 		KeyValuePair res = new KeyValuePair();
 		res.setKey(key);
 		res.setValue(value);
 		return res;
 	}
+
+	private List<KeyValuePair> list = new ArrayList<>();
+
+	public static KeyValuePairFactory create(KeyValuePairEnum key, String value) {
+		KeyValuePairFactory res = new KeyValuePairFactory();
+		res.add(key, value);
+		return res;
+	}
+
+	public KeyValuePairFactory add(KeyValuePairEnum key, String value) {
+		this.list.add(createKey(key.getKey(), value));
+		return this;
+	}
+
+	public KeyValuePair[] asArray() {
+		return this.list.toArray(new KeyValuePair[list.size()]);
+	}
+
+	public static KeyValuePairFactory createDocumentKeys(String typeId, String docName, String codEnte, String codAoo) {
+		return create(DocumentKeyValuePairEnum.TYPE_ID, typeId).add(DocumentKeyValuePairEnum.DOCNAME, docName)
+				.add(DocumentKeyValuePairEnum.COD_ENTE, codEnte).add(DocumentKeyValuePairEnum.COD_AOO, codAoo);
+	}
+
 }
