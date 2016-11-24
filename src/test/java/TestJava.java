8fa4230764afca26c7449a8012e066afc4890c3f
Mirco <amirco@gmail.com>
2016-11-24 16:40:16 +0100

    aggiunto test generico java

---
+++ src/test/java/TestJava.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class TestJava {
+
+	@Test
+	public void test() {
+		List<String> res = Arrays.asList(null);
+		Assert.assertNull(res);
+	}
+
+}
