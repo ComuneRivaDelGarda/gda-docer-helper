b5a375fb021da0d985dd10b9e1ad48a471b0938a
Mirco <amirco@gmail.com>
2016-11-24 18:13:10 +0100

    aggiunto protocollazione

---
+++ src/main/java/it/tn/rivadelgarda/comune/gda/docer/DocerProtocollazioneHelper.java
@@ -0,0 +1,9 @@
+package it.tn.rivadelgarda.comune.gda.docer;
+
+public class DocerProtocollazioneHelper extends AbstractDocerHelper {
+
+	public DocerProtocollazioneHelper(String docerSerivcesUrl, String docerUsername, String docerPassword) {
+		super(docerSerivcesUrl, docerUsername, docerPassword);
+	}
+
+}
