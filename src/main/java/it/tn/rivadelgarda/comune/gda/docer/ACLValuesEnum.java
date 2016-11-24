46d752fa7b56cb600282e32c7039182a5a694b19
Mirco <amirco@gmail.com>
2016-11-24 16:40:02 +0100

    aggiunti valori acls

---
+++ src/main/java/it/tn/rivadelgarda/comune/gda/docer/ACLValuesEnum.java
@@ -0,0 +1,21 @@
+package it.tn.rivadelgarda.comune.gda.docer;
+
+public enum ACLValuesEnum implements KeyValuePairEnum {
+	READ_ONLY_ACCESS(0), NORMAL_ACCESS(1), FULL_ACCESS(2);
+
+	private int value;
+
+	private ACLValuesEnum(int value) {
+		this.value = value;
+	}
+
+	public int getValue() {
+		return value;
+	}
+
+	@Override
+	public String getKey() {
+		return String.valueOf(value);
+	}
+
+}
