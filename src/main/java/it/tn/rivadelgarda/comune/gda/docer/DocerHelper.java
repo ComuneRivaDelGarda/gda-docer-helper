f77199c1b73aada9ee4ddf94588a0e55260fb700
Mirco <amirco@gmail.com>
2016-11-24 20:26:22 +0100

    merge metodi su docerhelper

--- src/main/java/it/tn/rivadelgarda/comune/gda/docer/DocerHelper.java
+++ src/main/java/it/tn/rivadelgarda/comune/gda/docer/DocerHelper.java
@@ -9,24 +9,37 @@ import javax.activation.DataHandler;
 import javax.activation.FileDataSource;
 
 import org.apache.commons.codec.digest.DigestUtils;
+import org.apache.commons.io.FileUtils;
 import org.apache.commons.lang3.StringUtils;
 
 import it.kdm.docer.webservices.DocerServicesStub;
+import it.kdm.docer.webservices.DocerServicesStub.AddNewVersion;
+import it.kdm.docer.webservices.DocerServicesStub.AddNewVersionResponse;
 import it.kdm.docer.webservices.DocerServicesStub.AddRelated;
 import it.kdm.docer.webservices.DocerServicesStub.AddRelatedResponse;
 import it.kdm.docer.webservices.DocerServicesStub.AddToFolderDocuments;
 import it.kdm.docer.webservices.DocerServicesStub.AddToFolderDocumentsResponse;
+import it.kdm.docer.webservices.DocerServicesStub.ArchiviaDocumento;
+import it.kdm.docer.webservices.DocerServicesStub.ArchiviaDocumentoResponse;
+import it.kdm.docer.webservices.DocerServicesStub.ClassificaDocumento;
+import it.kdm.docer.webservices.DocerServicesStub.ClassificaDocumentoResponse;
 import it.kdm.docer.webservices.DocerServicesStub.CreateDocument;
 import it.kdm.docer.webservices.DocerServicesStub.CreateDocumentResponse;
 import it.kdm.docer.webservices.DocerServicesStub.CreateFolder;
 import it.kdm.docer.webservices.DocerServicesStub.CreateFolderResponse;
 import it.kdm.docer.webservices.DocerServicesStub.DeleteDocument;
 import it.kdm.docer.webservices.DocerServicesStub.DeleteDocumentResponse;
+import it.kdm.docer.webservices.DocerServicesStub.DownloadVersion;
+import it.kdm.docer.webservices.DocerServicesStub.DownloadVersionResponse;
 import it.kdm.docer.webservices.DocerServicesStub.GetACLDocument;
 import it.kdm.docer.webservices.DocerServicesStub.GetACLDocumentResponse;
 import it.kdm.docer.webservices.DocerServicesStub.GetFolderDocuments;
 import it.kdm.docer.webservices.DocerServicesStub.GetFolderDocumentsResponse;
+import it.kdm.docer.webservices.DocerServicesStub.GetVersions;
+import it.kdm.docer.webservices.DocerServicesStub.GetVersionsResponse;
 import it.kdm.docer.webservices.DocerServicesStub.KeyValuePair;
+import it.kdm.docer.webservices.DocerServicesStub.ProtocollaDocumento;
+import it.kdm.docer.webservices.DocerServicesStub.ProtocollaDocumentoResponse;
 import it.kdm.docer.webservices.DocerServicesStub.RemoveFromFolderDocuments;
 import it.kdm.docer.webservices.DocerServicesStub.RemoveFromFolderDocumentsResponse;
 import it.kdm.docer.webservices.DocerServicesStub.SearchFolders;
@@ -34,17 +47,20 @@ import it.kdm.docer.webservices.DocerServicesStub.SearchFoldersResponse;
 import it.kdm.docer.webservices.DocerServicesStub.SearchItem;
 import it.kdm.docer.webservices.DocerServicesStub.SetACLDocument;
 import it.kdm.docer.webservices.DocerServicesStub.SetACLDocumentResponse;
+import it.kdm.docer.webservices.DocerServicesStub.StreamDescriptor;
+import it.tn.rivadelgarda.comune.gda.docer.keys.DocerCostant;
 import it.tn.rivadelgarda.comune.gda.docer.keys.DocumentKeysEnum;
+import it.tn.rivadelgarda.comune.gda.docer.keys.FolderKeysEnum;
 import it.tn.rivadelgarda.comune.gda.docer.keys.DocumentKeysEnum.TIPO_COMPONENTE;
 import it.tn.rivadelgarda.comune.gda.docer.values.ACLValuesEnum;
-import it.tn.rivadelgarda.comune.gda.docer.keys.FolderKeysEnum;
-import it.tn.rivadelgarda.comune.gda.docer.keys.DocerCostant;
 
-public class DocerVersamentoHelper extends AbstractDocerHelper {
+public class DocerHelper extends AbstractDocerHelper {
 
-	public DocerVersamentoHelper(String docerSerivcesUrl, String docerUsername, String docerPassword) {
+	public DocerHelper(String docerSerivcesUrl, String docerUsername, String docerPassword) {
 		super(docerSerivcesUrl, docerUsername, docerPassword);
 	}
+
+	/** VERSAMENTO */
 
 	/**
 	 * Questo metodo permette la creazione di una Folder nel DMS.
@@ -64,8 +80,7 @@ public class DocerVersamentoHelper extends AbstractDocerHelper {
 		 * docerCodiceAOO);
 		 */
 		KeyValuePair[] folderinfo = KeyValuePairFactory.build(FolderKeysEnum.FOLDER_NAME, folderName)
-				.add(FolderKeysEnum.COD_ENTE, docerCodiceENTE)
-				.add(FolderKeysEnum.COD_AOO, docerCodiceAOO).get();
+				.add(FolderKeysEnum.COD_ENTE, docerCodiceENTE).add(FolderKeysEnum.COD_AOO, docerCodiceAOO).get();
 
 		DocerServicesStub service = new DocerServicesStub(docerSerivcesUrl + DocerServices);
 		CreateFolder request = new CreateFolder();
@@ -323,6 +338,178 @@ public class DocerVersamentoHelper extends AbstractDocerHelper {
 		request.setDocId(documentId);
 		request.setRelated(related);
 		AddRelatedResponse response = service.addRelated(request);
+		boolean esito = response.get_return();
+		return esito;
+	}
+
+	/** VERSIONAMENTO */
+
+	/**
+	 * Questo metodo permette di creare una nuova versione del file (o documento
+	 * elettronico) di un Documento nel DMS. Il metodo è applicabile per la sola
+	 * gestione del versioning standard.
+	 * 
+	 * @param documentsId
+	 *            id del Documento
+	 * @param file
+	 *            La nuova versione del file o documento elettronico
+	 * @return Il version number della versione creata
+	 * @throws Exception
+	 */
+	public String addNewVersion(String documentsId, File file) throws Exception {
+		FileDataSource fileDataSource = new FileDataSource(file);
+		DataHandler dataHandler = new DataHandler(fileDataSource);
+
+		DocerServicesStub service = new DocerServicesStub(docerSerivcesUrl + DocerServices);
+		AddNewVersion request = new AddNewVersion();
+		request.setToken(getLoginTicket());
+		request.setDocId(documentsId);
+		request.setFile(dataHandler);
+		AddNewVersionResponse response = service.addNewVersion(request);
+		String version = response.get_return();
+		return version;
+	}
+
+	/**
+	 * Questo metodo permette di recuperare la lista dei version number ovvero
+	 * delle versioni dei file di un Documento del DMS. Il metodo è applicabile
+	 * per la sola gestione del versioning standard.
+	 * 
+	 * @param documentsId
+	 *            id del Documento di riferimento
+	 * @return collezione dei version number
+	 * @throws Exception
+	 */
+	public String[] getVersions(String documentsId) throws Exception {
+		DocerServicesStub service = new DocerServicesStub(docerSerivcesUrl + DocerServices);
+		GetVersions request = new GetVersions();
+		request.setToken(getLoginTicket());
+		request.setDocId(documentsId);
+		GetVersionsResponse response = service.getVersions(request);
+		String[] versions = response.get_return();
+		return versions;
+	}
+
+	/**
+	 * Questo metodo permette di recuperare una specifica versione del file (o
+	 * documento elettronico) di un Documento nel DMS.
+	 * 
+	 * @param documentsId
+	 *            id del Documento
+	 * @param versionNumber
+	 *            Version number del file
+	 * @return L’oggetto StreamDescriptor contiene una variabile “byteSize” di
+	 *         tipo Long che rappresenta la dimensione del file e una variabile
+	 *         “handler” che rappresenta il file o documento elettronico
+	 *         relativo alla versione richiesta
+	 * @throws Exception
+	 */
+	public StreamDescriptor downloadVersion(String documentsId, String versionNumber) throws Exception {
+		DocerServicesStub service = new DocerServicesStub(docerSerivcesUrl + DocerServices);
+		DownloadVersion request = new DownloadVersion();
+		request.setToken(getLoginTicket());
+		request.setDocId(documentsId);
+		request.setVersionNumber(versionNumber);
+		DownloadVersionResponse response = service.downloadVersion(request);
+		StreamDescriptor res = response.get_return();
+		return res;
+	}
+
+	/**
+	 * 
+	 * @param documentsId
+	 *            documentsId id del Documento
+	 * @param versionNumber
+	 *            Version number del file
+	 * @param file
+	 *            file di destinazione
+	 * @throws Exception
+	 */
+	public void downloadVersionTo(String documentsId, String versionNumber, File file) throws Exception {
+		StreamDescriptor data = downloadVersion(documentsId, versionNumber);
+		long size = data.getByteSize();
+		DataHandler dh = data.getHandler();
+		FileUtils.copyInputStreamToFile(dh.getInputStream(), file);
+	}
+
+	/** PROTOCOLLAZIONE */
+
+	/**
+	 * Questo metodo permette l’assegnazione dei metadati di protocollazione ad
+	 * un Documento del DMS
+	 * 
+	 * @param documentId
+	 *            La variabile docId è l’id del Documento nel DMS.
+	 * @param related
+	 *            L’oggetto metadata[] è una collezione di nodi metadata. Ogni
+	 *            nodo metadata contiene una KeyValuePair ovvero due nodi, key e
+	 *            value, di tipo string dove i valori ammessi per i nodi key
+	 *            sono i nomi dei metadati del profilo relativi alla
+	 *            protocollazione di un documento(si veda paragrafo 4.4. Profilo
+	 *            di un documento).
+	 * @return true se l’operazione è andata a buon fine
+	 * @throws Exception
+	 */
+	public boolean protocollaDocumento(String documentId, KeyValuePair[] metadata) throws Exception {
+		DocerServicesStub service = new DocerServicesStub(docerSerivcesUrl + DocerServices);
+		ProtocollaDocumento request = new ProtocollaDocumento();
+		request.setToken(getLoginTicket());
+		request.setDocId(documentId);
+		request.setMetadata(metadata);
+		ProtocollaDocumentoResponse response = service.protocollaDocumento(request);
+		boolean esito = response.get_return();
+		return esito;
+	}
+
+	/**
+	 * Questo metodo permette la classificazione di un Documento e di tutti i
+	 * suoi related nel DMS.
+	 * 
+	 * @param documentId
+	 *            id del Documento
+	 * @param metadata
+	 *            L’oggetto metadata[] è una collezione di nodi metadata. Ogni
+	 *            nodo metadata contiene una KeyValuePair ovvero due nodi, key e
+	 *            value, di tipo string dove i valori ammessi per i nodi key
+	 *            sono i nomi dei metadati del profilo relativi alla
+	 *            classificazione (si veda paragrafo 4.4. Profilo di un
+	 *            documento).
+	 * @return
+	 * @throws Exception
+	 */
+	public boolean classificaDocumento(String documentId, KeyValuePair[] metadata) throws Exception {
+		DocerServicesStub service = new DocerServicesStub(docerSerivcesUrl + DocerServices);
+		ClassificaDocumento request = new ClassificaDocumento();
+		request.setToken(getLoginTicket());
+		request.setDocId(documentId);
+		request.setMetadata(metadata);
+		ClassificaDocumentoResponse response = service.classificaDocumento(request);
+		boolean esito = response.get_return();
+		return esito;
+	}
+
+	/**
+	 * Questo metodo permette l’archiviazione (in archivio di deposito) di un
+	 * Documento e di tutti i suoi related nel DMS.
+	 * 
+	 * @param documentId
+	 *            id del Documento
+	 * @param metadata
+	 *            L’oggetto metadata[] è una collezione di nodi metadata. Ogni
+	 *            nodo metadata contiene una KeyValuePair ovvero due nodi, key e
+	 *            value, di tipo string dove i valori ammessi per i nodi key
+	 *            sono i nomi dei metadati del profilo relativi all’archivio di
+	 *            deposito (si veda paragrafo 4.4. Profilo di un documento).
+	 * @return
+	 * @throws Exception
+	 */
+	public boolean archiviaDocumento(String documentId, KeyValuePair[] metadata) throws Exception {
+		DocerServicesStub service = new DocerServicesStub(docerSerivcesUrl + DocerServices);
+		ArchiviaDocumento request = new ArchiviaDocumento();
+		request.setToken(getLoginTicket());
+		request.setDocId(documentId);
+		request.setMetadata(metadata);
+		ArchiviaDocumentoResponse response = service.archiviaDocumento(request);
 		boolean esito = response.get_return();
 		return esito;
 	}
