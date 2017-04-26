933499efa95b3611deb9273a90c4fd3d0608655a
Mirco <amirco@gmail.com>
2017-04-26 11:08:28 +0200

    corretto stile javadoc

--- src/main/java/it/tn/rivadelgarda/comune/gda/docer/keys/MetadatiDocumento.java
+++ src/main/java/it/tn/rivadelgarda/comune/gda/docer/keys/MetadatiDocumento.java
@@ -17,19 +17,19 @@ import java.util.List;
  *
  */
 public enum MetadatiDocumento implements MetadatoDocer {
-	
+
 	/**
 	 * Metadati di base generici:
 	 */
-	
-    /**
-     * codice dell’Ente assegnato al documento
-     */
-    COD_ENTE(COD_ENTE_KEY),
-    /**
-     * codice della AOO assegnata al documento
-     */
-    COD_AOO(COD_AOO_KEY),
+
+	/**
+	 * codice dell’Ente assegnato al documento
+	 */
+	COD_ENTE(COD_ENTE_KEY),
+	/**
+	 * codice della AOO assegnata al documento
+	 */
+	COD_AOO(COD_AOO_KEY),
 	/**
 	 * obbligatorio, il document-type del documento. è il tipo di documento,
 	 * individua l’insieme delle proprietà del profilo
@@ -58,15 +58,22 @@ public enum MetadatiDocumento implements MetadatoDocer {
 
 	/**
 	 * indica lo stato “archivistico” di un documento, è un tipo enumerato i cui
-	 * valori ammissibili sono: 0 (generico document) 1 (generico definitivo), 2
-	 * (registrato), 3 (protocollato), 4 (classificato), 5 (fascicolato), 6 (in
-	 * archivio di deposito)
+	 * valori ammissibili sono:
+	 * <li>0 (generico document)
+	 * <li>1 (generico definitivo)
+	 * <li>2 (registrato)
+	 * <li>3 (protocollato)
+	 * <li>4 (classificato)
+	 * <li>5 (fascicolato)
+	 * <li>6 (in archivio di deposito)
 	 */
 	STATO_ARCHIVISTICO(STATO_ARCHIVISTICO_KEY),
 	/**
 	 * indica lo stato avanzamento nel workflow di protocollazione/registrazione
-	 * di un documento, è un tipo enumerato i cui valori ammissibili sono: 0
-	 * (non definito) 1 (da protocollare), 2 (da fascicolare)
+	 * di un documento, è un tipo enumerato i cui valori ammissibili sono:
+	 * <li>0 (non definito)
+	 * <li>1 (da protocollare)
+	 * <li>2 (da fascicolare)
 	 */
 	STATO_BUSINESS(STATO_BUSINESS_KEY),
 	/**
@@ -107,16 +114,16 @@ public enum MetadatiDocumento implements MetadatoDocer {
 	 */
 	UD_VERSION(UD_VERSION_KEY),
 	/**
-	 * METADATO aggiunto il 16/03/2017 per memorizzare ID VERSANTE e fare 
-	 * successive ricerche per documenti appartenenti alla stessa ENTITA' del VERSANTE
+	 * METADATO aggiunto il 16/03/2017 per memorizzare ID VERSANTE e fare
+	 * successive ricerche per documenti appartenenti alla stessa ENTITA' del
+	 * VERSANTE
 	 */
 	EXTERNAL_ID(EXTERNAL_ID_KEY),
 
-	
 	/*
 	 * Metadati di classificazione e fascicolazione:
 	 */
-	
+
 	/**
 	 * è la classifica del Titolario assegnato al documento
 	 */
@@ -137,73 +144,71 @@ public enum MetadatiDocumento implements MetadatoDocer {
 	 * “1.3.5/2012/1/2/1/3;1.3.6/2012/3/1”).
 	 */
 	FASC_SECONDARI(FASC_SECONDARI_KEY),
-	
+
 	/*
 	 * Metadati di protocollazione:
 	 */
-	
-    /**
-     * numero di protocollo assegnato al documento
-     */
-    NUM_PG(NUM_PG_KEY),
-    /**
-     * anno di protocollo assegnato al documento, se assente viene ricavata da
-     * DATA_PG; se specificato deve coincidere con l’anno presente in DATA_PG
-     */
-    ANNO_PG(ANNO_PG_KEY),
-    /**
-     * oggetto del protocollo
-     */
-    OGGETTO_PG(OGGETTO_PG_KEY),
-    /**
-     * registro del protocollo
-     */
-    REGISTRO_PG(REGISTRO_PG_KEY),
-    /**
-     * data di protocollazione
-     */
-    DATA_PG(DATA_PG_KEY),
-    /**
-     * tipo protocollazione
-     * <p>
-     * <ul>
-     * <li>E (entrata)</li>
-     * <li>I (interna)</li>
-     * <li>U (uscita)</li>
-     * <li>ND (non definita)</li>
-     * </ul>
-     * </p>
-     */
-    TIPO_PROTOCOLLAZIONE(TIPO_PROTOCOLLAZIONE_KEY),
-    /**
-     * i mittenti (è anche metadato di Registrazione)
-     */
-    MITTENTI(MITTENTI_KEY),
-    /**
-     * i destinatari (è anche metadato di Registrazione)
-     */
-    DESTINATARI(DESTINATARI_KEY),
-    /**
-     * tipo firma (è anche metadato di Registrazione)
-     */
-    TIPO_FIRMA(TIPO_FIRMA_KEY),
-    /**
-     * tfirmatario : firmatario (è anche metadato di Registrazione)
-     */
-    FIRMATARIO(FIRMATARIO_KEY);
-    
+
+	/**
+	 * numero di protocollo assegnato al documento
+	 */
+	NUM_PG(NUM_PG_KEY),
+	/**
+	 * anno di protocollo assegnato al documento, se assente viene ricavata da
+	 * DATA_PG; se specificato deve coincidere con l’anno presente in DATA_PG
+	 */
+	ANNO_PG(ANNO_PG_KEY),
+	/**
+	 * oggetto del protocollo
+	 */
+	OGGETTO_PG(OGGETTO_PG_KEY),
+	/**
+	 * registro del protocollo
+	 */
+	REGISTRO_PG(REGISTRO_PG_KEY),
+	/**
+	 * data di protocollazione
+	 */
+	DATA_PG(DATA_PG_KEY),
+	/**
+	 * tipo protocollazione
+	 * <p>
+	 * <li>E (entrata)</li>
+	 * <li>I (interna)</li>
+	 * <li>U (uscita)</li>
+	 * <li>ND (non definita)</li>
+	 * </p>
+	 */
+	TIPO_PROTOCOLLAZIONE(TIPO_PROTOCOLLAZIONE_KEY),
+	/**
+	 * i mittenti (è anche metadato di Registrazione)
+	 */
+	MITTENTI(MITTENTI_KEY),
+	/**
+	 * i destinatari (è anche metadato di Registrazione)
+	 */
+	DESTINATARI(DESTINATARI_KEY),
+	/**
+	 * tipo firma (è anche metadato di Registrazione)
+	 */
+	TIPO_FIRMA(TIPO_FIRMA_KEY),
+	/**
+	 * tfirmatario : firmatario (è anche metadato di Registrazione)
+	 */
+	FIRMATARIO(FIRMATARIO_KEY);
+
 	/**
 	 * Metadati per la conservazione sostitutiva:
 	 */
-    
-    /**
-     * Metadati di Registrazione
-     */
-    
-    /**
-     * Metadati di Pubblicazione:
-     */
-    
+
+	/**
+	 * Metadati di Registrazione
+	 */
+
+	/**
+	 * Metadati di Pubblicazione:
+	 */
+
 	private String key;
 
 	private MetadatiDocumento(final String key) {
@@ -240,14 +245,15 @@ public enum MetadatiDocumento implements MetadatoDocer {
 			return value;
 		}
 	}
-	
+
 	/**
 	 * 
 	 * @author mirco
 	 *
 	 */
 	public enum TIPO_COMPONENTE_VALUES implements MetadatoDocer {
-		PRINCIPALE(TIPO_COMPONENTE_PRINCIPALE), ALLEGATO(TIPO_COMPONENTE_ALLEGATO), ANNESSO(TIPO_COMPONENTE_ANNESSO), ANNOTAZIONE(TIPO_COMPONENTE_ANNOTAZIONE);
+		PRINCIPALE(TIPO_COMPONENTE_PRINCIPALE), ALLEGATO(TIPO_COMPONENTE_ALLEGATO), ANNESSO(
+				TIPO_COMPONENTE_ANNESSO), ANNOTAZIONE(TIPO_COMPONENTE_ANNOTAZIONE);
 		private String value;
 
 		private TIPO_COMPONENTE_VALUES(String value) {
